f03e62fb2c913f437517aa06658a443ef87409d8
HENRY MUTEGA <hmutega@gmail.com>
2024-12-06 11:05:46 +0300

    User structures for API models.

--- services/user.service.go
+++ services/user.service.go
@@ -1,1 +1,11 @@
 package services
+
+import "log.com/log-apis/models"
+
+type UserService interface {
+	CreateUser(*models.User) error
+	GetUser(*string) (*models.User, error)
+	GetAll() ([]*models.User, error)
+	UpdateUser(*models.User)
+	DeleteUser(*string) error
+}
